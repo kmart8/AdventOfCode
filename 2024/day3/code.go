c01b94e3d4040e1cc36e4c9e5923c0f4ab8c5ed8
Kevin Martin <kevinjskk19@gmail.com>
2024-12-03 12:23:27 -0500

    refactoring day 3

--- 2024/day3/code.go
+++ 2024/day3/code.go
@@ -6,42 +6,31 @@ import (
 	"regexp"
 )
 
-func p1() int {
-	file, _ := os.ReadFile("./inputs/day3.txt")
-	strs := regexp.MustCompile(`mul\(\d+,\d+\)`).FindAllString(string(file), -1)
-	ans := 0
-	for _, v := range strs {
-		var a, b int
-		fmt.Sscanf(v, "mul(%d,%d)", &a, &b)
-		ans += a * b
-	}
-	return ans
-}
-
-func p2() int {
+func day3() (int, int) {
 	file, _ := os.ReadFile("./inputs/day3.txt")
 	strs := regexp.MustCompile(`mul\(\d+,\d+\)|do\(\)|don't\(\)`).FindAllString(string(file), -1)
 	multiply := true
-	ans := 0
+	ans1 := 0
+	ans2 := 0
 	for _, v := range strs {
 		if v == "do()" {
 			multiply = true
 		} else if v == "don't()" {
 			multiply = false
 		} else {
+			var a, b int
+			fmt.Sscanf(v, "mul(%d,%d)", &a, &b)
+			ans1 += a * b
 			if multiply {
-				var a, b int
-				fmt.Sscanf(v, "mul(%d,%d)", &a, &b)
-				ans += a * b
+				ans2 += a * b
 			}
 		}
 	}
-	return ans
+	return ans1, ans2
 }
 
 func main() {
-	ans1 := p1()
-	ans2 := p2()
+	ans1, ans2 := day3()
 	fmt.Println(fmt.Sprintf("The answer to day 3 part 1 is %d", ans1))
 	fmt.Println(fmt.Sprintf("The answer to day 3 part 2 is %d", ans2))
 }
