a69779015463a69f3c829a4acd00aef72f182898
Kevin Martin <kevinjskk19@gmail.com>
2024-12-04 23:24:58 -0500

    refactoring day 4

--- 2024/day4/code.go
+++ 2024/day4/code.go
@@ -11,7 +11,7 @@ func valid(x int, y int) bool {
 	return (x >= 0 && x < 140) && (y >= 0 && y < 140)
 }
 
-func p1() int {
+func buildMatrix() [140][]string {
 	file, _ := os.Open("./inputs/day4.txt")
 	scanner := bufio.NewScanner(file)
 	// build the matrix
@@ -24,6 +24,24 @@ func p1() int {
 		}
 		i++
 	}
+	return matrix
+}
+
+func findXmas(direction [2][]int, x int, y int, matrix [140][]string) []string {
+	curr := []string{}
+	for _, coord := range direction {
+		dx := coord[0]
+		dy := coord[1]
+		newX, newY := x+dx, y+dy
+		if valid(newX, newY) && (matrix[newY][newX] == "M" || matrix[newY][newX] == "S") {
+			curr = append(curr, matrix[newY][newX])
+		}
+	}
+	return curr
+}
+
+func p1() int {
+	matrix := buildMatrix()
 
 	directions := [8][]int{
 		{0, 1},
@@ -57,24 +75,11 @@ func p1() int {
 			}
 		}
 	}
-
 	return ans
-
 }
 
 func p2() int {
-	file, _ := os.Open("./inputs/day4.txt")
-	scanner := bufio.NewScanner(file)
-	// build the matrix
-	matrix := [140][]string{}
-	i := 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		for _, v := range line {
-			matrix[i] = append(matrix[i], string(v))
-		}
-		i++
-	}
+	matrix := buildMatrix()
 
 	// each direction must contain an 'M' & 'S'
 	direction1 := [2][]int{
@@ -90,33 +95,15 @@ func p2() int {
 	for y, row := range matrix {
 		for x, v := range row {
 			if v == "A" {
-				curr1 := []string{}
-				curr2 := []string{}
-				for _, coord := range direction1 {
-					dx := coord[0]
-					dy := coord[1]
-					newX, newY := x+dx, y+dy
-					if valid(newX, newY) && (matrix[newY][newX] == "M" || matrix[newY][newX] == "S") {
-						curr1 = append(curr1, matrix[newY][newX])
-					}
-				}
-				for _, coord := range direction2 {
-					dx := coord[0]
-					dy := coord[1]
-					newX, newY := x+dx, y+dy
-					if valid(newX, newY) && (matrix[newY][newX] == "M" || matrix[newY][newX] == "S") {
-						curr2 = append(curr2, matrix[newY][newX])
-					}
-				}
+				curr1 := findXmas(direction1, x, y, matrix)
+				curr2 := findXmas(direction2, x, y, matrix)
 				if (slices.Contains(curr1, "M") && slices.Contains(curr1, "S")) && (slices.Contains(curr2, "M") && slices.Contains(curr2, "S")) {
 					ans += 1
 				}
 			}
 		}
 	}
-
 	return ans
-
 }
 
 func main() {
